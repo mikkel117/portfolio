cadc81ff68c8bd0c0d8f750ba52b1995eb828654
rumbo <rumbo.9370@gmail.com>
2022-01-18 13:39:07 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./App.scss";
 
 import Nav from "./component/header-footer/nav";
@@ -6,6 +7,9 @@ import Cookie from "./component/page/Cookie";
 import FetchContextProvider from "./contexts/Fetch";
 
 function App() {
+  useEffect(() => {
+    document.title = "Portfolio";
+  }, []);
   return (
     <>
       <FetchContextProvider>
