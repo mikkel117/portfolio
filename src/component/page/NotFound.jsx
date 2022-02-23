e96095ec59fe79be0d9c79759743c671ace62b60
rumbo <rumbo.9370@gmail.com>
2022-02-23 14:00:22 +0100

    update

--- src/component/page/NotFound.jsx
+++ src/component/page/NotFound.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <section>
+    <section className='notFound'>
       <h1>404</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <Link to='/'>Go back to the homepage</Link>
+      <p>
+        Go back to the <Link to='/'>homepage</Link>
+      </p>
     </section>
   );
 }
